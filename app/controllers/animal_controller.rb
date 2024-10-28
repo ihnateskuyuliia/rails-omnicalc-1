ef21aa4c4234f541e24ef8c41a515eab112d720b
ihnateskuyuliia <yuliiaihnatesku@uchicago.edu>
2024-10-28 18:13:13 +0000

    root done

--- app/controllers/animal_controller.rb
+++ app/controllers/animal_controller.rb
@@ -1,17 +1,30 @@
 class AnimalController < ApplicationController
 
+  #MAIN PAGE (OPENS AT SQUARE BUT WITH NO /SQUARE/NEW)
   def main_page
-    render({ :template => "game_templates/main_page"})
+    render({ :template => "calc_templates/main_page"})
   end
   
+  #SQUARE
   def square_new
-    render({ :template => "game_templates/new_square_calc"})
+    render({ :template => "calc_templates/new_square_calc"})
   end
 
   def square_results
     @the_num = params.fetch("users_number").to_f
     @the_result = @the_num ** 2
-    render({ :template => "game_templates/new_square_results"})
+    render({ :template => "calc_templates/new_square_results"})
   end
+
+  #ROOT
+  def root_new
+    render({ :template => "calc_templates/root_calc"})
+  end
+
+  def root_results
+    @the_num = params.fetch("users_number").to_f
+    @the_result = Math.sqrt(@the_num)
+    render({ :template => "calc_templates/root_results"})
+  end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get("/square/new", { :controller => "animal", :action => "square_new" })
   get("/square/results", { :controller => "animal", :action => "square_results" })
 
+  get("/square_root/new", { :controller => "animal", :action => "root_new" })
+  get("/square_root/results", { :controller => "animal", :action => "root_results" })
+
 end
