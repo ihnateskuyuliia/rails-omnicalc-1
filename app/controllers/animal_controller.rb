47558f3dc6bc836f9665050ffe5fc2d86b1a2da2
ihnateskuyuliia <yuliiaihnatesku@uchicago.edu>
2024-10-28 16:23:17 +0000

    yay

--- app/controllers/animal_controller.rb
+++ app/controllers/animal_controller.rb
@@ -4,4 +4,14 @@ class AnimalController < ApplicationController
     render({ :template => "game_templates/main_page"})
   end
   
+  def square_new
+    render({ :template => "game_templates/new_square_calc"})
+  end
+
+  def square_results
+    @the_num = params.fetch("users_number").to_f
+    @the_result = @the_num ** 2
+    render({ :template => "game_templates/new_square_results"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "animal", :action => "main_page" })
-  #get("/square/new", { :controller => "animal", :action => "square" })
+
+  get("/square/new", { :controller => "animal", :action => "square_new" })
+  get("/square/results", { :controller => "animal", :action => "square_results" })
+
 end
